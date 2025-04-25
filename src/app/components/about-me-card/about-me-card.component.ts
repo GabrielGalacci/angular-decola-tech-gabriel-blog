270bdfd5f930a8adb7fcac3d65ad0c77a8eb93a1
Gabriel Galacci <gabrielcssoad@hotmail.com>
2025-04-25 18:47:28 -0300

    Creating Basic Structure for de Blog-Site

---
+++ src/app/components/about-me-card/about-me-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me-card',
+  imports: [],
+  templateUrl: './about-me-card.component.html',
+  styleUrl: './about-me-card.component.css'
+})
+export class AboutMeCardComponent {
+
+}
