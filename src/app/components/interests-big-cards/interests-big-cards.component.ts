63abe9936c5ad65229ec573826cbe941dfb2d576
Gabriel Galacci <gabrielcssoad@hotmail.com>
2025-04-25 23:19:22 -0300

    Creating Cards and Big Cards

---
+++ src/app/components/interests-big-cards/interests-big-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interests-big-cards',
+  imports: [],
+  templateUrl: './interests-big-cards.component.html',
+  styleUrl: './interests-big-cards.component.css'
+})
+export class InterestsBigCardsComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BlogTitleComponent } from "../../components/blog-title/blog-title.component";
 import { AboutMeCardComponent } from "../../components/about-me-card/about-me-card.component";
+import { CardsComponent } from "../../components/cards/cards.component";
+import { InterestsBigCardsComponent } from "../../components/interests-big-cards/interests-big-cards.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BlogTitleComponent, AboutMeCardComponent],
+  imports: [BlogTitleComponent, AboutMeCardComponent, CardsComponent, InterestsBigCardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
